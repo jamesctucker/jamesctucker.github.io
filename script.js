7cea0dcb6e745d33918452bcde277c29cc9e59e5
James Tucker <43385166+jamesctucker@users.noreply.github.com>
2018-09-25 15:25:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ function openTab(evt, tabName) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-       <button class="tablinks" onclick="openTab(event, 'Summary')" id="Summary">Summary</button>
+    
+<button class="tablinks" onclick="openTab(event, 'Summary')" id="DefaultOpen">Summary</button>
 
 <script>
-// Get the element with id="Summary" and click on it
-document.getElementById("Summary").click();
+// Get the element with id="DefaultOpen" and click on it
+document.getElementById("DefaultOpen").click();
 </script> 
 
     // Get all elements with class="tablinks" and remove the class "active"
