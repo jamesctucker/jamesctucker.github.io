f661d179debba4725ec5addd7036c0e5530c9fde
James Tucker <43385166+jamesctucker@users.noreply.github.com>
2018-09-25 14:56:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
+    
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
